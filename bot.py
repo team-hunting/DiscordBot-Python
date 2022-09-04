815b29e868a0b25ce2858f8ca38c9afb7712fd98
Travis Hunting <58845455+TravisHunting@users.noreply.github.com>
2022-09-05 10:58:22 +1200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,73 @@
+import discord
+import os
+import random
+from discord.ext import commands
+from dotenv import load_dotenv
+from insult import insults
+
+load_dotenv()
+
+intents = discord.Intents(
+    messages=True, 
+    guilds=True, 
+    members=True, 
+    message_content=True
+    )
+
+client = discord.Bot(intents=intents)
+bot = commands.Bot()
+token = os.getenv('TOKEN')
+
+
+def generate_insult():
+    adj = random.choice(insults['adjectives'])
+    noun = random.choice(insults['nouns'])
+    vowels = 'aeiou'
+    if adj[0] in vowels:
+        return f"is an {adj} {noun}."
+    return f"is a {adj} {noun}."
+
+
+@client.event
+async def on_ready():
+	print(f"Logged in as a bot {client.user}")
+
+
+@bot.command()
+async def getmsg(ctx):
+
+    ref_msg = ctx.reference
+    channel = client.get_guild(ref_msg.guild_id).get_channel(ref_msg.channel_id)
+    message = await channel.fetch_message(ref_msg.message_id)
+
+    return message
+
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    print(f'Message {user_message} by {username} on {channel}') 
+
+    if "!insult" in user_message:
+        if message.reference is not None:
+
+            replymsg = await getmsg(message)
+            original_author = replymsg.author
+
+            insult = f"{original_author.name} {generate_insult()}"
+
+            # await message.channel.send(f"hello {original_author}", reference=message)
+            await message.channel.send(f"{insult}", reference=message)
+
+        return
+
+
+
+client.run(token)
---
+++ insult.py
@@ -0,0 +1,51 @@
+insults = {
+
+'adjectives':[
+    'below average','boisterous','average','abnormal','childish','overemotional','hedonistic',
+    'wretched','tactless','imperfect','melodramatic','unimpressive','indiscreet','domineering',
+    'manipulative','voiceless','barefooted','self-absorbed','unappetizing','massive','brutal',
+    'dishonorable','severe','corrupt','gullible','slimy','mediocre','weighty','wasteful','chaotic',
+    'cruel','eye-popping','unsupported','bureaucratic','dislikeable','inoperative','small-minded',
+    'nonrenewable','incomprehensible','illogical','obnoxious','ignorant','insane','gross','vicious',
+    'bad-tempered','dogmatic','ruthless','worthless','pristine','clingy','irresponsible','deceitful',
+    'bigoted','hypercritical','unattractive','complete','smelly','gormless','inexperienced','repulsive',
+    'depressed','untidy','machiavellian','gluttonous','unwanted','incomplete','dirty','bewitched','unreliable',
+    'absolutely','inflexible','insidious','questionable','pompous','grumpy','ordinary','corrosive','cold-hearted',
+    'quick-tempered','depressed','gigantic','moody','uncommonly','uncivilized','colossal','giggling','nasty','sad',
+    'patronising','depressed','dishonest','cynical','dishonest','unwise','unpredictable','shell-shocked','concrete',
+    'poisonous','pessimistic','exceedingly','disconnected','drunken','worthless','unbelievable','remarkable',
+    'thoughtless','judgmental','fragile','antisocial','nightmarish','dismissive','impatient','tense','emotionless',
+    'foolhardy','bitter','petty','depressing','nihilistic','incomplete','stubborn','flimsy','unhealthy','abrasive',
+    'narcissistic','immature','malevolent','clammy','cowardly','smug','weak-willed','apathetically','dysfunctional',
+    'unwanted','disturbing','foolish','idle','organic','seismic','narrow-minded','dense','impolite','lousy','supercilious',
+    'possessive','whining','aggressive','inconsiderable','sobbing','apathetic','disposable','patronizing','uncooked','pig-headed',
+    'intolerant','filthy','disappointing','inconsiderate','petite','banal','plain','anxious','unceremonious','artificial',
+    'discriminating','lowlife','egocentric','lackadaisical','grim','flabby','expired','condescending','morbid','self-centred',
+    'laughable','callous','brutal','powerless','grim','unfuckable','untidy','careless','neurotic','confrontational','ghastly',
+    'psychotic','flaky','inhuman','untrustworthy','bitchy','crummy','vacuous','thoughtless','obnoxious','unpleasant','vulgar',
+    'rude','suspicious','offensive','tremendous'
+    ],
+
+'nouns':[
+    'tranny','bitch','spaghettibender','jizz guzzler','camelfucker','poopface','oxygen thief',
+    'cumshot','motherfucker','scumfuck','abortion','shit-eater','cunt','badgerfucker','tampon',
+    'syphilis','son of a motherless goat','pussy','lowlife','jackwagon','douche canoe','dickcheese',
+    'pikey','slag','mingebag','rabbitfucker','cocksucker','unseasoned cabbage','jerk','twatwaffle',
+    'retard','thundercunt','cum dumpster','galah','dipstick','low-life','nutjob','toe eyed cabbage',
+    'fucknut','fish fucker','ballsack','incel','ass peddler','butt-banger','snatchpatch','mouth breather',
+    'gonorrehea','twat','sycophant','prick','dicklicker','simp','shitnugget','schmuck','pussy pisser','pudwhacker',
+    'slag','chickenfucker','ape covered in human flesh','ass-nugget','donkey','pissbaby','cousinfucker','birdbrain',
+    'tosser','pork','blubber gut','terrorist','beginner','junglebunny','slimebucket','dingleberry','wanker','scum',
+    'beat-off','poontang','crotchmonkey','dumbass','trailertrash','dingaling','skank','ass clown','creep','muppet',
+    'shitkicker','pooperscooper','bastard','porchmonkey','oompa loompa','Meg Griffin','dingbat','testicle','birdfucker',
+    'bonehead','assbag','dickbreath','failed abortion','cum guzzler','degenerate','fucktard','dickweasel','landwhale','cuckold',
+    'cockburger','spermherder','bitchzilla','fuckbucket','uphill gardener','shitweasel','plonker','scumface','pelvic inflammatory disease',
+    'fannyfucker','turd cutter','minx','unclefucker','nutfucker','shitforbrains','genital herpes','limpdick','fungus','shitbreath','ejaculation',
+    'pillock','nobody','disease','mattressprincess','hamster','flatulence','hippie','nitwit','piss monkey','pornprincess','gobshite',
+    'sheepfucker','garbage','peasant','inbred','hillbillie','neanderthal','mucky pup','shitpuddle','keyboard warrior',
+    'shitgoblin','zero','idiot sandwich','tree hugger','gorilla','gatorbait','cockqueen','sissy','sheepshagger',
+    'sewer rat','tonguetramp','fruitcake','penispuffer','fuck nugget','snowflake','nonce','slutbag','village idiot',
+    'orphan','horseshit','scumbreath','mongoose','swine','redneck','numbnuts','wallflower','window licker','herpes',
+    'zitface','zebrafucker','sleezebag','scrotum','saggy fuck','doo-doo','sphincter','sausage-masseuse','tosser','yonker'
+    ]
+}
